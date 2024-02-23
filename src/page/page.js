a9e4287b19c9bfe15fd7d7403fee38315b2747a4
jalenlum <808jalen@gmail.com>
2024-02-22 21:23:18 -1000

    added resume

--- src/page/page.js
+++ src/page/page.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Navbar } from "../components/Navbar";
 import { Home } from "../components/Home";
 import { About } from "../components/About";
-// import { Resume } from "../components/Resume";
+import { Resume } from "../components/Resume";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 
@@ -13,7 +13,7 @@ export const Page = () => {
       <Navbar />
       <Home />
       <About />
-      {/* <Resume /> */}
+      <Resume />
       <Projects />
       <Contact />
     </>
